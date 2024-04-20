bcdd12056214ba160fa29a49ea032f9b94b25556
Beka <enuqidzebeqa@gmail.com>
2024-04-20 15:08:20 +0400

    chrome driver update

---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+    @Test
+    void Setup(){
+        System.setProperty("chrome.driver", "Resources");
+        WebDriver driver = new ChromeDriver();
+    driver.get("https://www.youtube.com/");
+    }
+}
