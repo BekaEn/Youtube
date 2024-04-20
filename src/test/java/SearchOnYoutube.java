c739a195802b31503c9372c5c71907286d03cd4a
Beka <enuqidzebeqa@gmail.com>
2024-04-20 17:14:37 +0400

    deleted:    src/test/Resources/chromedriver 	renamed:    src/test/java/test.java -> src/test/java/SearchOnYoutube.java

---
+++ src/test/java/SearchOnYoutube.java
@@ -0,0 +1,27 @@
+import java.time.Duration;
+import ChromeRunner.ChromeRunner;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+
+public class SearchOnYoutube extends ChromeRunner{
+    @Test
+    public void testSearch() {
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+        
+        // Wait for the search box to become visible
+        WebElement searchBox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("search-input")));
+
+        // Find the actual input element within the search box
+        WebElement searchInput = searchBox.findElement(By.id("search"));
+
+        // Type a search query into the search box
+        searchInput.sendKeys("Beka Enukidze");
+
+        // Submit the form
+        searchInput.submit();
+    }
+}
