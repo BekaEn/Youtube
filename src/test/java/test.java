2e4714ccb884070f88b3eea22a863e8f5bb1a0ab
Beka <enuqidzebeqa@gmail.com>
2024-04-23 06:31:27 +0400

    modified:   src/test/java/test.java 	modified:   test-output/YoutubeSuite/YoutubeTest.html 	modified:   test-output/YoutubeSuite/YoutubeTest.xml 	modified:   test-output/emailable-report.html 	modified:   test-output/index.html 	modified:   test-output/junitreports/TEST-YoutubeTest.xml 	modified:   test-output/testng-results.xml

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -2,7 +2,7 @@ import org.testng.TestNG;
 
 import java.util.Collections;
 
-public class test {
+public class Test {
     public static void main(String[] args) {
         TestNG testng = new TestNG();
         testng.setTestSuites(Collections.singletonList("testng.xml"));
